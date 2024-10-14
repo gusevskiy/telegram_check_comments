a024e0f1fffa2d44e55ac2fdda7b67a0353adc1e
пгыуgusevskiy <vlgu@mail.ru>
2024-10-14 22:03:31 +0300

    Добавил блокировку пользователя

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# teelgram_check_comments
-Проверка комментариев в телеграмм
+# Descriptions (описание)
+Бот мониторит сообщения в телеграмм группе
+
+
+## Solves the problem (для каких задач)
+Текст сообщения прогоняется через модель "text-classification" для определения в нем негатива.
+
+
+### Deploy (развертывание)
+```bash
+
+```
+
+```bash
+
+```
+
+
+
+### Run (запуск)
+```bash
+cd lap_top_scrapy
+
+scrapy crawl lap_top -O lap_top.json
+```
+ 
+
+
+#### Technologies (технологии)
+python==3.12  
+ 
+
+#### Plans for completion (планы/мечты)
+* 
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.types import ChatPermissions
+from datetime import datetime, timedelta
 
 from handlers.basic import model_check_text
 from utils.log_config import setup_logger
@@ -10,18 +12,25 @@ from utils.log_config import setup_logger
 load_dotenv()
 logger = setup_logger()
 
-api_id = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-chat_id = int(os.getenv('CHAT_ID'))
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+chat_id = int(os.getenv("CHAT_ID"))
+block_time_hour = os.getenv("BLOCK_TIME_HOUR")
+
 
 # client = Client(name='my_client', api_id=api_id, api_hash=api_hash)
 
-app = Client("my_client", bot_token=os.getenv('TOKEN'))
+app = Client("my_client", bot_token=os.getenv("TOKEN"))
+
+
+# async def work_block(app, message):
+
+
 
 @app.on_message(filters.chat(chats=chat_id))
 async def get_message(app, message: Message):
     """
-    Приложение мониторит группу chat_id 
+    Приложение мониторит группу chat_id
     каждое сообщение отправляет в модель
     """
     try:
@@ -29,6 +38,7 @@ async def get_message(app, message: Message):
         check, result = await model_check_text(message.text)
         # У некоторых пользователей нет username, тч лучше использовать имя
         user_name = message.from_user.first_name
+        user_id = message.from_user.id
         if check:
             # пишем в лог только отпределеные как негатив
             # вообще можно наверно копить таким образом dataset и потом на нем модель обучать
@@ -36,18 +46,16 @@ async def get_message(app, message: Message):
             logger.info(f"{check} -> {result} -> {message.text[:30]}")
             # предупреждение для пользователя, по идее нужно логику блокировки делать.
             await message.reply(f"{user_name} -> You toxic, I'm blocking you")
+            # блокируем человека на 2 минуты
+            await app.restrict_chat_member(
+                chat_id,
+                user_id,
+                ChatPermissions(),
+                datetime.now() + timedelta(minutes=block_time_hour),
+            )
     except Exception as e:
         logger.error(f"app error: {str(e)}")
 
 
-# Одно из решений чтобы бота не добавляли другие пользователи.
-# по ка эффективность не понятна
-@app.on_chat_member_updated()
-async def check_admin_status(app, message: Message):
-    chat_member = await app.get_chat_member(message.chat.id, app.me.id)
-    if not chat_member.status == "administrator":
-        await app.leave_chat(message.chat.id)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
